f7c6ab12539664afb6b3864a3eb1093aa4a9cbb4
Carl-eric HOLM <cholm@e2r1p5.42.fr>
2018-06-12 14:22:06 +0200

    change

--- srcs/lem_in_findbest.c
+++ srcs/lem_in_findbest.c
@@ -6,7 +6,7 @@
 /*   By: femaury <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 13:06:20 by femaury           #+#    #+#             */
-/*   Updated: 2018/06/12 12:27:57 by femaury          ###   ########.fr       */
+/*   Updated: 2018/06/12 14:17:00 by cholm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,18 @@
 
 static int	find_end(t_env *env, t_room *curr, t_list *tunnel, unsigned len)
 {
-	ft_printf("Send help\n");
-	fflush(NULL);
 	if (curr)
 	{
-		ft_printf("Currently in room %s\n", curr->name);
-		fflush(NULL);
 		curr->status = curr->status == EMPTY ? OCCUPIED : curr->status;
 		tunnel = curr->links;
 		if (ft_lstfind_content(tunnel, env->end))
 		{
-			ft_printf("Found link to END\n");
-			fflush(NULL);
 			if (len < env->best_len)
 			{
-				ft_printf("Found shorter path.\n");
-				fflush(NULL);
 				env->best_len = len;
+				ft_lstappend(&env->tmp, ft_lstnew(curr->name, ft_strlen(curr->name)));
 				env->best = ft_lstcpy(&env->tmp);
+				ft_lstdellast(&env->tmp);
 				ft_lstdellast(&env->tmp);
 			}
 			curr->status = EMPTY;
@@ -52,14 +46,14 @@ static int	find_end(t_env *env, t_room *curr, t_list *tunnel, unsigned len)
 		{
 			if (find_room(env->rooms, tunnel->content)->status == EMPTY)
 			{
-				ft_lstappend(&env->tmp, ft_lstnew(curr->name, ft_strlen(curr->name)));
+				if (curr != find_room(env->rooms, env->start)
+						&& !ft_lstfind_content(env->tmp, curr->name))
+					ft_lstappend(&env->tmp, ft_lstnew(curr->name, ft_strlen(curr->name)));
 				find_end(env, find_room(env->rooms, tunnel->content), NULL, len + 1);
 			}
 			tunnel = tunnel->next;
 		}
 		curr->status = curr->status == OCCUPIED ? EMPTY : curr->status;
-		ft_printf("toasties\n");
-		fflush(NULL);
 		ft_lstdellast(&env->tmp);
 	}
 	return (0);
