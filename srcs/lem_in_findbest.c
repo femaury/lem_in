567222d6a4bfb7ea9ab8f6a3f2e7111c2cfc38a7
Felix MAURY <femaury@student.42.fr>
2018-06-14 14:11:51 +0200

    Normified findbest.c and fixed bug when START -> END

--- srcs/lem_in_findbest.c
+++ srcs/lem_in_findbest.c
@@ -6,20 +6,41 @@
 /*   By: femaury <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 13:06:20 by femaury           #+#    #+#             */
-/*   Updated: 2018/06/14 12:41:37 by femaury          ###   ########.fr       */
+/*   Updated: 2018/06/14 14:11:02 by femaury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
 /*
- *		int	find_end(t_env *env, t_room *curr, t_list *tunnel, unsigned len);
+ *		void	save_path(t_env *env, t_room *curr, unsigned len);
  *
- *	Recursive function that finds the lenght of the shortest path from start to
- *	end and saves it to env->best_len.
+ *	Adds current room to end of tmp list. Saves shortest path found by
+ *	copying env->tmp to env->best and saves its length in env->best_len.
+ *	Removes current room from tmp list and sets it back to EMPTY (backtrack).
  *
- *	TO DO:
- *		Save in env->best the names of all the rooms in the shortest path found.
+*/
+
+static void	save_path(t_env *env, t_room *curr, unsigned len)
+{
+	if (len < env->best_len)
+	{
+		env->best_len = len;
+		if (!ft_lstfind_content(env->tmp, curr->name))
+			ft_lstappend(&env->tmp, ft_lstnew(curr->name,
+					ft_strlen(curr->name)));
+		env->best = ft_lstcpy(&env->tmp);
+		ft_lstdellast(&env->tmp);
+	}
+	curr->status = EMPTY;
+}
+
+/*
+ *		int		find_end(t_env *env, t_room *curr, t_list *tunnel, unsigned len);
+ *
+ *	Recursive function that checks all possible paths from START to END and calls
+ *	save_path every time it finds a shorter path. Each time you enter a room,
+ *	you set it to OCCUPIED and save the previous room in a list (env->tmp).
  *
 */
 
@@ -31,24 +52,18 @@ static void	find_end(t_env *env, t_room *curr, t_list *tunnel, unsigned len)
 		tunnel = curr->links;
 		if (ft_lstfind_content(tunnel, env->end))
 		{
-			if (len < env->best_len)
-			{
-				env->best_len = len;
-				ft_lstappend(&env->tmp, ft_lstnew(curr->name, ft_strlen(curr->name)));
-				env->best = ft_lstcpy(&env->tmp);
-				ft_lstdellast(&env->tmp);
-			}
-			curr->status = EMPTY;
+			save_path(env, curr, len);
 			return ;
 		}
 		while (tunnel)
 		{
 			if (find_room(env->rooms, tunnel->content)->status == EMPTY)
 			{
-				if (curr != find_room(env->rooms, env->start)
-						&& !ft_lstfind_content(env->tmp, curr->name))
-					ft_lstappend(&env->tmp, ft_lstnew(curr->name, ft_strlen(curr->name)));
-				find_end(env, find_room(env->rooms, tunnel->content), NULL, len + 1);
+				if (!ft_lstfind_content(env->tmp, curr->name))
+					ft_lstappend(&env->tmp, ft_lstnew(curr->name,
+								ft_strlen(curr->name)));
+				find_end(env, find_room(env->rooms,
+							tunnel->content), NULL, len + 1);
 			}
 			tunnel = tunnel->next;
 		}
